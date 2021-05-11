9acd7dc4e0c7640bd4d437adc456729ecd4bcb29
cuza22 <yeji.woo97@gmail.com>
2021-05-11 15:21:44 +0900

    timer

--- DataCollectingController.swift
+++ DataCollectingController.swift
@@ -8,5 +8,38 @@
 import UIKit
 
 class DataCollectingViewController: UIViewController {
-    @IBOutlet var textTime: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    var timer : Timer?
+    var timeLeft : Int = 5
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(DataCollectingViewController.timerCallBack), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerCallBack() {
+        timeLeft -= 1
+        timeLabel.text = String(timeLeft) + "seconds remaining..."
+        
+        if timeLeft == 0 {
+            timer?.invalidate()
+            end()
+        }
+    }
+    
+    func end() {
+        let mainStoryBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let Survey2View = mainStoryBoard.instantiateViewController(identifier: "Survey2")
+        
+        Survey2View.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        Survey2View.modalPresentationStyle = .overFullScreen
+        
+        self.present(Survey2View, animated: true)
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+    }
 }
