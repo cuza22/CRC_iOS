032746ffb1b4ae1ddf9e7f4a014702313a95ac5a
cuza22 <yeji.woo97@gmail.com>
2021-07-05 03:13:43 +0900

    frequency error fixed

--- DataCollect.swift
+++ DataCollect.swift
@@ -13,7 +13,7 @@ let FREQUENCY: Double = 60
 let GPS_INTERVAL: Double = 5
 let WRITE_INTERVAL: Double = 5
 let GRAVITY_CONST: Double = -9.81
-let PRESSURE_CONST: Double = 10
+let PRESSURE_CONST: Double = 100
 let fileCreator = FileCreator()
 
 @available(iOS 14.0, *)
@@ -23,9 +23,9 @@ class DataCollect : CMMotionManager,
 
     // Motion Data
     var motionManager = CMMotionManager() // gravity, linear acceleration, accelerometer, gyroscope, magnetometer
-    var attitude = CMAttitude() // rotation vector, orientation
+//    var attitude = CMAttitude() // rotation vector, orientation
     var altimeter = CMAltimeter() // pressure, height
-    var pedometer = CMPedometerEvent() // step
+    var pedometer = CMPedometer() // step
     var sensorDataArray: [Double] = []
     let sensorHeader = "Time,Year,Month,Day,Hour,Min,Sec,GraX,GraY,GraZ,LinearAccX,LinearAccY,LinearAccZ,GyroX,GyroY,GyroZ,Height,Proxi,MagX,MagY,MagZ,Light,Step,Pressure,RotVec_0,RotVec_1,RotVec_2,RotVec_3,RotVec_4,Orientation_0_Azimuth,Orientation_1_Pitch,Orientation_2_Roll,AccX,AccY,AccZ,Mode,Survey1\n"
     var sensorDataString : String = ""
@@ -51,99 +51,105 @@ class DataCollect : CMMotionManager,
             self.motionManager.startAccelerometerUpdates()
             self.motionManager.startGyroUpdates()
             self.motionManager.startMagnetometerUpdates()
+            self.startStepUpdates()
+            self.startAltimeterUpdates()
             UIDevice.current.isProximityMonitoringEnabled = true
-            altimeter.startRelativeAltitudeUpdates(to: .main, withHandler: { (altiData, error) in
-                if (error == nil) {
-                    
-                    self.sensorTimer = Timer(fire: Date(), interval: (1/FREQUENCY), repeats: true, block: { (timer) in
-                        if let motion = self.motionManager.deviceMotion,
-                           let acc = self.motionManager.accelerometerData,
-                           let gyro = self.motionManager.gyroData,
-                           let mag = self.motionManager.magnetometerData {
-                            self.sensorDataArray = [
-                                // Gra
-                                motion.gravity.x * GRAVITY_CONST,
-                                motion.gravity.y * GRAVITY_CONST,
-                                motion.gravity.z * GRAVITY_CONST,
-                                // LinearAcc
-                                motion.userAcceleration.x * GRAVITY_CONST,
-                                motion.userAcceleration.y * GRAVITY_CONST,
-                                motion.userAcceleration.z * GRAVITY_CONST,
-                                // Gyro
-                                gyro.rotationRate.x,
-                                gyro.rotationRate.y,
-                                gyro.rotationRate.z,
-                                // Height
-                                altiData!.relativeAltitude.doubleValue,
-                                // Proxi
-                                self.getProximity(),
-                                // Mag
-                                mag.magneticField.x,
-                                mag.magneticField.y,
-                                mag.magneticField.z,
-                                // Light
-                                0,
-                                // Step
-                                0,
-                                // Pressure
-                                altiData!.pressure.doubleValue * PRESSURE_CONST,
-                                // RotVec
-                                motion.attitude.quaternion.w,
-                                motion.attitude.quaternion.x,
-                                motion.attitude.quaternion.y,
-                                motion.attitude.quaternion.z,
-                                0, // RotVec_4
+
+            self.sensorTimer = Timer(fire: Date(), interval: (1/FREQUENCY), repeats: true, block: { (timer) in
+                if let motion = self.motionManager.deviceMotion,
+                   let acc = self.motionManager.accelerometerData,
+                   let gyro = self.motionManager.gyroData,
+                   let mag = self.motionManager.magnetometerData {
+                    self.sensorDataArray = [
+                        // Gra
+                        motion.gravity.x * GRAVITY_CONST,
+                        motion.gravity.y * GRAVITY_CONST,
+                        motion.gravity.z * GRAVITY_CONST,
+                        // LinearAcc
+                        motion.userAcceleration.x * GRAVITY_CONST,
+                        motion.userAcceleration.y * GRAVITY_CONST,
+                        motion.userAcceleration.z * GRAVITY_CONST,
+                        // Gyro
+                        gyro.rotationRate.x,
+                        gyro.rotationRate.y,
+                        gyro.rotationRate.z,
+                        // Height
+                        self.altimeterData?.relativeAltitude.doubleValue ?? 0,
+                        // Proxi
+                        UIDevice.current.proximityState ? 8 : 0,
+                        // Mag
+                        mag.magneticField.x,
+                        mag.magneticField.y,
+                        mag.magneticField.z,
+                        // Light
+                        -1,
+                        // Step
+                        self.pedometerEventType,
+                        // Pressure
+                        self.altimeterData?.pressure.doubleValue ?? 0 * PRESSURE_CONST,
+                        // RotVec
+                        motion.attitude.quaternion.w,
+                        motion.attitude.quaternion.x,
+                        motion.attitude.quaternion.y,
+                        motion.attitude.quaternion.z,
+                        0, // RotVec_4
 //                                self.attitude.quaternion.w,
 //                                self.attitude.quaternion.x,
 //                                self.attitude.quaternion.y,
 //                                self.attitude.quaternion.z,
-                                // Orientation
-                                motion.attitude.yaw,
-                                motion.attitude.pitch,
-                                motion.attitude.roll,
+                        // Orientation
+                        motion.attitude.yaw,
+                        motion.attitude.pitch,
+                        motion.attitude.roll,
 //                                self.attitude.yaw,
 //                                self.attitude.pitch,
 //                                self.attitude.roll,
-                                // Acc
-                                acc.acceleration.x * GRAVITY_CONST,
-                                acc.acceleration.y * GRAVITY_CONST,
-                                acc.acceleration.z * GRAVITY_CONST
-                            ]
-                            self.sensorDataString += self.dateManager(type: "data") + self.sensorDataArray.map({"\($0)"}).joined(separator: ",")
+                        // Acc
+                        acc.acceleration.x * GRAVITY_CONST,
+                        acc.acceleration.y * GRAVITY_CONST,
+                        acc.acceleration.z * GRAVITY_CONST
+                    ]
+                    self.sensorDataString += self.dateManager(type: "data") + self.sensorDataArray.map({"\($0)"}).joined(separator: ",")
 
-                            // add Mode and Survey1 "only once"
-                            if (self.count_sensor > 0) {
-                                let modeAndSurvey: String =  "," + self.ad!.selectedTransportation + "," + self.ad!.position
-                                self.sensorDataString += modeAndSurvey
-                                self.count_sensor -= 1
-                            }
-                            self.sensorDataString += "\n"
-                        }
-                    })
-                    
-                    RunLoop.current.add(self.sensorTimer!, forMode: .default)
+                    // add Mode and Survey1 "only once"
+                    if (self.count_sensor > 0) {
+                        let modeAndSurvey: String =  "," + self.ad!.selectedTransportation + "," + self.ad!.position
+                        self.sensorDataString += modeAndSurvey
+                        self.count_sensor -= 1
+                    }
+                    self.sensorDataString += "\n"
                 }
             })
-        }
-    }
-        
-    // proximity
-    func getProximity() -> Double {
-        if UIDevice.current.proximityState {
-            return 8.0
-        } else {
-            return 0.0
+            RunLoop.current.add(self.sensorTimer!, forMode: .default)
         }
     }
     
+    var pedometerEventType: Double = 0
+    func startStepUpdates() {
+        self.pedometer.startEventUpdates(handler: {
+            (event: CMPedometerEvent?, error: Error?) in
+            if event!.type == .pause {
+                self.pedometerEventType = 0
+            } else {
+                self.pedometerEventType = 1
+            }
+        })
+     }
+
+    var altimeterData: CMAltitudeData?
+    func startAltimeterUpdates() {
+        self.altimeter.startRelativeAltitudeUpdates(to: .main, withHandler: { (data, error) in
+            self.altimeterData = data
+        })
+    }
+    
     func endGetSensorData() -> Void {
-//        let sensorFileURL = fileCreator.createFileURL(transportation: ad!.selectedTransportation, sensor: "SensorData", directoryURL: (ad?.directoryURL)!)
-//        fileCreator.writeInFile(csvString: self.sensorDataString, fileURL: sensorFileURL) // data
         self.motionManager.stopDeviceMotionUpdates()
         self.motionManager.stopAccelerometerUpdates()
         self.motionManager.stopGyroUpdates()
         self.motionManager.stopMagnetometerUpdates()
         self.altimeter.stopRelativeAltitudeUpdates()
+        self.pedometer.stopEventUpdates()
         UIDevice.current.isProximityMonitoringEnabled = false
     }
     
@@ -163,7 +169,7 @@ class DataCollect : CMMotionManager,
         if (locationManager.authorizationStatus == .authorizedAlways
                 || locationManager.authorizationStatus == .authorizedWhenInUse) {
             locationManager.delegate = self
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest // highest accuracy
+//            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest // highest accuracy
             self.locationManager.startUpdatingLocation()
         }
         
@@ -192,24 +198,18 @@ class DataCollect : CMMotionManager,
     }
 
     func endGetGPSData() -> Void {
-//        let GPSFileURL = fileCreator.createFileURL(transportation: ad!.selectedTransportation, sensor: "GPSData", directoryURL: (ad?.directoryURL)!)
-//        fileCreator.writeInFile(csvString: self.GPSDataString, fileURL: GPSFileURL)
         self.locationManager.stopUpdatingLocation()
     }
     
     var writeTimer: Timer?
     func writeInFile() -> Void {
-        print("write in file start\n")
         // file URL
         let sensorFileURL = fileCreator.createFileURL(transportation: ad!.selectedTransportation, sensor: "SensorData", directoryURL: (ad?.directoryURL)!)
         let GPSFileURL = fileCreator.createFileURL(transportation: ad!.selectedTransportation, sensor: "GPSData", directoryURL: (ad?.directoryURL)!)
-        print(GPSFileURL)
         // write once in five seconds
         self.writeTimer = Timer(fire: Date(), interval: WRITE_INTERVAL, repeats: true, block: { (timer) in
-            print("write\n")
             fileCreator.writeInFile(csvString: self.sensorDataString, fileURL: sensorFileURL)
             fileCreator.writeInFile(csvString: self.GPSDataString, fileURL: GPSFileURL)
-            print(self.GPSDataString)
         })
         RunLoop.current.add(self.writeTimer!, forMode: .default)
     }
